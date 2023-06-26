3d887ed78b8fbea0ad1854342532a26d06f5a780
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2023-06-26 18:08:25 -0400

    more basic structuring getting to setup connection to db with orm

--- src/commands/moderation/setup.js
+++ src/commands/moderation/setup.js
@@ -1,13 +1,50 @@
-module.exports = {
-    name: "setup",
-    description: "Add your regiment to the App",
-    aliases: ["init", "addreg"],
-    category: "Moderation",
-    isAdmin: true, 
-    execute(message, args) {
-      // Add your database regiment logic here
+const { EmbedBuilder } = require('discord.js');
 
-      message.channel.send("Initialize Setup of Regiment")
+module.exports = {
+  name: "setup",
+  description: "Add your regiment to the App",
+  aliases: ["init", "addreg"],
+  category: "Moderation",
+  isAdmin: true,
+  async execute(message, args) {
+    try {
+      let invite = await message.channel.createInvite({
+        maxAge: 0, // 0 = infinite expiration
+        maxUses: 0 // 0 = infinite uses
+      }).catch(console.error);
+
+      const guildId = message.guild.id;
+      const guildAvatar = message.guild.iconURL();
+      const authorId = message.author.id;
+
+      let member = message.mentions.members.first();
+
+      let memberx = message.member;
+      let serverNickname = memberx ? memberx.displayName : null;
+
+      const embed = new EmbedBuilder()
+        .setColor("#7c7d72")
+        .setTitle("Regiment Setup Information")
+        .addFields({ name: "Guild ID", value: guildId })
+        .addFields({ name: "Guild Name", value: message.guild.name })
+        .addFields({ name: "Guild Avatar", value: guildAvatar })
+        .addFields({ name: "Guild Invite", value: invite.url })
+        .addFields({ name: "Regiment Owner Discord ID", value: authorId })
+        .addFields({ name: "Regiment Owner Nickname", value: serverNickname })
+        .setTimestamp();
+
+      console.log(`Guild ID: ${guildId}`);
+      console.log(`Guild Name: ${message.guild.name}`);
+      console.log(`Guild Avatar: ${guildAvatar}`);
+      console.log(`Guild Invite: ${invite.url}`);
+      console.log(`Author ID: ${authorId}`);
+      console.log(`Server Nickname: ${serverNickname}`);
+
+      message.channel.send({ embeds: [embed] });
+      message.channel.send("Initialize Regiment Model, Sequelize, and Database.");
+    } catch (error) {
+      console.error(error);
+      message.channel.send("An error occurred while setting up the regiment.");
     }
-  };
-  
+  }
+};
