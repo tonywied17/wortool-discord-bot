d2dd5df1351b5ddef24bcaf6f1bbc5e06457ca76
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2024-02-12 14:23:05 -0500

    isAdmin and isRoleManager flag checks implemented. help command updated as well.

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -3,18 +3,24 @@ const path = require('path');
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField } = require('discord.js');
 
 module.exports = {
+    isAdmin: false,
+    isRoleManager: false,
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription('Lists all available commands sorted by category and provides useful web links.'),
     async execute(interaction) {
-        const commandsPath = '/home/bots/ReggieBot/commands';
-		console.log(commandsPath)
+        const commandsPath = path.join(__dirname, '../../commands');
+        console.log(commandsPath);
         const categories = fs.readdirSync(commandsPath).filter(file => fs.statSync(path.join(commandsPath, file)).isDirectory());
 
         const embed = new EmbedBuilder()
             .setColor('#0099ff')
             .setTitle('Available Commands')
             .setDescription('Here are the commands sorted by category:');
+
+        const member = interaction.guild.members.cache.get(interaction.user.id);
+        const hasAdminPermissions = member.permissions.has(PermissionsBitField.Flags.Administrator);
+        const hasWorToolManagerRole = member.roles.cache.some(role => role.name === "WoRTool Manager");
 
         for (const category of categories) {
             const categoryCommandsPath = path.join(commandsPath, category);
@@ -25,53 +31,43 @@ module.exports = {
             for (const file of commandFiles) {
                 const filePath = path.join(categoryCommandsPath, file);
                 const command = require(filePath);
-                if (command.data.name) {
-                    commandList += `\`/${command.data.name}\`   ${command.data.description || 'No description available'}\n`;
+
+                const displayCommand = hasAdminPermissions || 
+                    (command.isRoleManager && hasWorToolManagerRole) || 
+                    (!command.isAdmin && !command.isRoleManager); 
+
+                if (command.data.name && displayCommand) {
+                    const commandDescription = `\`/${command.data.name}\` ${command.data.description || 'No description available'}`;
+                    commandList += `${commandDescription}\n`;
                 }
             }
 
             if (commandList.length > 0) {
-                embed.addFields({ name: category.charAt(0).toUpperCase() + category.slice(1), value: commandList });
+                embed.addFields({ name: category.charAt(0).toUpperCase() + category.slice(1), value: commandList.trim() });
             }
         }
 
-        const webLinksRow = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setLabel('Manage Regiment')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL('https://wortool.com/mod/1'),
-                new ButtonBuilder()
-                    .setLabel('Event Share Tool')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL('https://wortool.com/mod/2'),
-                new ButtonBuilder()
-                    .setLabel('Steam Stats Tracker')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL('https://wortool.com/mod/3'),
-                new ButtonBuilder()
-                    .setLabel('Company Muster Tool')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL('https://wortool.com/mod/4')
-            );
+        embed.setFooter({ text: 'Commands available to you based on your permissions and roles are listed.' });
 
+        await interaction.reply({
+            embeds: [embed],
+            ephemeral: true
+        }).catch(console.error);
 
+        if (hasAdminPermissions || hasWorToolManagerRole) {
+            const webLinksRow = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder().setLabel('Manage Regiment').setStyle(ButtonStyle.Link).setURL('https://wortool.com/mod/1'),
+                    new ButtonBuilder().setLabel('Event Share Tool').setStyle(ButtonStyle.Link).setURL('https://wortool.com/mod/2'),
+                    new ButtonBuilder().setLabel('Steam Stats Tracker').setStyle(ButtonStyle.Link).setURL('https://wortool.com/mod/3'),
+                    new ButtonBuilder().setLabel('Company Muster Tool').setStyle(ButtonStyle.Link).setURL('https://wortool.com/mod/4')
+                );
 
-			await interaction.reply({
-				embeds: [embed],
-				ephemeral: true
-			}).catch(console.error);
-
-			const member = interaction.guild.members.cache.get(interaction.user.id);
-			if (member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-				await interaction.followUp({
-					content: `**WorTool Quick Links for Admins**\n`,
-					components: [webLinksRow],
-					ephemeral: true
-				}).catch(console.error);
-        	return;
-      		}
-			
-			
+            await interaction.followUp({
+                content: `**WorTool Quick Links for Admins & WoRTool Managers**\nIf you want allow additional users access to management commands and features give them the "WoRTool Manager" role.`,
+                components: [webLinksRow],
+                ephemeral: true
+            }).catch(console.error);
+        }
     },
 };
--- handlers/events/discord/interactionCreate.js
+++ handlers/events/discord/interactionCreate.js
@@ -1,52 +1,54 @@
-/*
- * File: c:\Users\tonyw\Desktop\git-222-bot\222-discord-bot\handlers\events\discord\interactionCreate.js
- * Project: c:\Users\tonyw\Desktop\ReggieBot\paapp2-discord-bot
- * Created Date: Saturday February 3rd 2024
- * Author: Tony Wiedman
- * -----
- * Last Modified: Fri February 9th 2024 10:28:24 
- * Modified By: Tony Wiedman
- * -----
- * Copyright (c) 2024 MolexWorks / Tone Web Design
- */
-
 const {
-	Events
+    Events,
+    PermissionsBitField
 } = require('discord.js');
 
 /**
  * Interaction Create Event
  * @type {import('discord.js').Event}
  * @description This event is emitted when a user uses a slash command.
- * Each command use has a 22% chance to send a waifu pic.
  * @param {import('discord.js').CommandInteraction} interaction
  * @returns {Promise<void>}
  */
 module.exports = {
-	name: Events.InteractionCreate,
-	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
-		const command = interaction.client.commands.get(interaction.commandName);
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
-		}
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({
-					content: 'There was an error while executing this command!',
-					ephemeral: true
-				});
-			} else {
-				await interaction.reply({
-					content: 'There was an error while executing this command!',
-					ephemeral: true
-				});
-			}
-		}
-	},
+    name: Events.InteractionCreate,
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return;
 
+        const command = interaction.client.commands.get(interaction.commandName);
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        if (command.isAdmin && !interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+            await interaction.reply({ content: "This command requires administrator permissions.", ephemeral: true });
+            return;
+        }
+
+        if (command.isRoleManager) {
+            let roleManager = interaction.guild.roles.cache.find(role => role.name === "WoRTool Manager");
+            if (!interaction.member.roles.cache.has(roleManager.id)) {
+                await interaction.reply({ content: "You do not have the 'WoRTool Manager' role.", ephemeral: true });
+                return;
+            }
+        }
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({
+                    content: 'There was an error while executing this command!',
+                    ephemeral: true
+                });
+            } else {
+                await interaction.reply({
+                    content: 'There was an error while executing this command!',
+                    ephemeral: true
+                });
+            }
+        }
+    },
 };
