31350509702706eea625d10c7c16d9ddfc8cfe0f
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2024-02-13 09:58:07 -0500

    markdown chars sanitized from usernames, formatting & embeds for commands

--- commands/regimental/promote.js
+++ commands/regimental/promote.js
@@ -1,16 +1,4 @@
-/*
- * File: c:\Users\tonyw\Desktop\ReggieBot\paapp2-discord-bot\commands\regimental\promote.js
- * Project: c:\Users\tonyw\Desktop\ReggieBot\paapp2-discord-bot
- * Created Date: Monday February 12th 2024
- * Author: Tony Wiedman
- * -----
- * Last Modified: Mon February 12th 2024 8:20:49 
- * Modified By: Tony Wiedman
- * -----
- * Copyright (c) 2024 MolexWorks / Tone Web Design
- */
-
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Permissions } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const axios = require('axios');
 require('dotenv').config();
 const bearerToken = process.env.AUTH_SECRET;
@@ -32,16 +20,10 @@ module.exports = {
       const regimentsResponse = await axios.get(`https://api.wortool.com/v2/regiments/g/${guildId}/discordGuild`, { headers: { Authorization: `Bearer ${bearerToken}` } });
       const regimentId = regimentsResponse.data.regimentId;
 
-      console.log(regimentId);
-
       const usersResponse = await axios.get(`https://api.wortool.com/v2/regiments/${regimentId}/users`, { headers: { Authorization: `Bearer ${bearerToken}` } });
       const users = usersResponse.data;
 
-      console.log(users);
-
       const targetUser = users.find(u => u.discordId === user.id);
-      
-      console.log(targetUser);
 
       if (targetUser) {
         await axios.put(`https://api.wortool.com/v2/auth/${targetUser.id}/setModeratorDiscord`, {}, { headers: { Authorization: `Bearer ${bearerToken}` } });
@@ -49,21 +31,43 @@ module.exports = {
 
       const role = interaction.guild.roles.cache.find(r => r.name === "WoRTool Manager");
       if (!role) {
-        return interaction.editReply("WoRTool Manager role does not exist in this server.");
+        return interaction.editReply({ content: "The WoRTool Manager role does not exist in this server.", ephemeral: true });
       }
       const member = await interaction.guild.members.fetch(user.id);
       if (!member.roles.cache.has(role.id)) {
         await member.roles.add(role);
       }
 
-      if (targetUser) {
-        await interaction.editReply(`<@${user.id}> has been promoted to a regimental manager and assigned the "WoRTool Manager" role.\n\nThey will need to **re-login** to wortool.com to access the web-based regimental management tools.`);
-      } else {
-        await interaction.editReply(`<@${user.id}>  has been assigned the "WoRTool Manager" role.\n\n They should sign up on wortool.com and sync their Discord & Regiment in the "Linked Accounts" section if they would like access to the web-based features. Otherwise, they can use the bot commands to manage the regiment.`);
-      }
+      const dmEmbed = new EmbedBuilder()
+        .setColor('#425678') 
+        .setTitle('You have been promoted!')
+        .setDescription(`Congratulations! You have been promoted to a regimental manager on ${interaction.guild.name}. Please re-login to [WoRTool](https://wortool.com/) to access the web-based features.`)
+        .addFields(
+          { name: 'Additional Management Options', value: 'The `/help` command will now show additional management options available to you. Make sure to use these tools to effectively manage your regiment.' }
+        )
+        .setTimestamp();
+
+      await user.send({ embeds: [dmEmbed] }).catch(error => console.error("Could not send DM to user.", error));
+
+      const replyEmbed = new EmbedBuilder()
+        .setColor('#425678') 
+        .setTitle('Promotion Success') 
+        .setDescription(targetUser ? `<@${user.id}> has been successfully promoted to a regimental manager.` : `<@${user.id}> has been assigned the "WoRTool Manager" role.`)
+        .addFields(
+          { name: 'Next Steps', value: targetUser ? 'Please have the user **re-login** to [wortool.com](https://wortool.com/) to access the web-based regimental management tools.' : 'Please sign up on [wortool.com](https://wortool.com/) and link your Discord account to access web-based features. You can now use bot commands to manage the regiment.' }
+        )
+        .setTimestamp();
+
+      await interaction.editReply({ embeds: [replyEmbed] });
     } catch (error) {
       console.error("Error promoting user:", error);
-      await interaction.editReply("An error occurred while trying to promote the user.");
+      const errorEmbed = new EmbedBuilder()
+        .setColor('#ff0000') 
+        .setTitle('Error Promoting User')
+        .setDescription('An error occurred while trying to promote the user. Please try again later.')
+        .setTimestamp(); 
+
+      await interaction.editReply({ embeds: [errorEmbed] });
     }
   },
 };
